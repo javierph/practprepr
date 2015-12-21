a3bbcf9886d03282b517a976d8abd7cabe065368
javierph <javierph@users.noreply.github.com>
2015-12-21 11:17:24 +0100

    commit inicial

---
+++ R/functions.R
@@ -0,0 +1,22 @@
+
+#' Suma los numeros pasados por parametro
+#' @param x Primer numero a sumar
+#' @param y Segundo numero a sumar
+#' @return La suma de \code{x} e \code{y}
+#' @examples
+#' suma.dos.numeros(13, 31)
+#' suma.dos.numeros(-1, 3.14159)
+suma.dos.numeros <- function(x, y) {
+  x + y
+}
+
+#' Resta los numeros pasados por parametro
+#' @param x Primer numero a restar
+#' @param y Segundo numero a restar
+#' @return La resta de \code{x} menos \code{y}
+#' @examples
+#' resta.dos.numeros(13, 31)
+#' resta.dos.numeros(-1, 3.14159)
+resta.dos.numeros <- function(x, y) {
+  x - y
+}
---
+++ R/practprepr.R
@@ -0,0 +1,12 @@
+#' practprepr: Paquete de practica
+#'
+#' Paquete para aprender como se crean, se distribuyen y se usan en R
+#' Contiene dos funciones sencillas
+#'
+#' @section practprepr functions:
+#' suma.dos.numeros
+#' resta.dos.numeros
+#'
+#' @docType package
+#' @name practprepr
+NULL
